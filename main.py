e1b074442be40fac24d384c54c76766e7eb16b7f
rasenmaeher92 <rasenmaeher92@users.noreply.github.com>
2020-03-20 23:26:58 +0100

    identify all contacts

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import numpy as np
 from scipy.spatial.distance import pdist, squareform
+from tqdm import tqdm
 
 # TODO: Use Argparse to get this from the commandline
-data = pd.read_csv("data.csv")
+data = pd.read_csv("simulation.csv")
 
 # determin start and end time
 time_start = data["t"].min()
 time_end = data["t"].max()
 
 # create dataframe for book keeping of contacts
-contacts = pd.DataFrame(columns=["id_a", "id_b", "t", "x", "y"])
+contacts = pd.DataFrame(columns=["agent_a", "agent_b", "t", "x", "y", "agent_a_state", "agent_b_state"])
 
 # iterate over time
-for t in range(time_start, time_end+1):
+for t in tqdm(range(time_start, time_end+1)):
     # filter for time and find pairwise distances
     current_data = data[data["t"]==t]
     pair_distances = pdist(current_data[["x","y"]], "euclidean")
@@ -21,11 +22,13 @@ for t in range(time_start, time_end+1):
     # use square form of pairwise distances and add diagonal to it, to avoid self matches
     con = np.argwhere(squareform(pair_distances)+np.eye(squareform(pair_distances).shape[0]) == 0)
     # derive contact and add it to contacts
-    contact = pd.DataFrame({"id_a": con[0][0],
-                            "id_b": con[0][1],
+    contact = pd.DataFrame({"agent_a": con[0][0],
+                            "agent_b": con[0][1],
                             "t": t,
-                            "x": current_data[current_data["id"] == con[0][0]]["x"].item(),
-                            "y": current_data[current_data["id"] == con[0][0]]["y"].item()}, index=[0])
+                            "x": current_data[current_data["agent"] == con[0][1]]["x"].item(),
+                            "y": current_data[current_data["agent"] == con[0][1]]["y"].item(),
+                            "agent_a_state": None,
+                            "agent_b_state": None,}, index=[0])
     contacts = pd.concat([contacts, contact])
 
 # sort table of ids by number of contacts
